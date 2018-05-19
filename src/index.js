cc98b6f9ce67db15c0b96f94c75efa6c5a2618bf
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-05-19 12:34:52 +0200

    initial structure setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import WebFont from "webfontloader";
+
+import Root from "./Root";
+import "./index.css";
+
+WebFont.load({
+  google: {
+    families: ["Montserrat:300,400,500,600,700"]
+  }
+});
+
+ReactDOM.render(<Root />, document.getElementById("root"));
