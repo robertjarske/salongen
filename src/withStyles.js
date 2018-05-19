76b9edcb1f8e077764a0ec6a99d710bf57925894
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-05-19 14:24:34 +0200

    add yarn.lock to .gitignore add withStyles.js

---
+++ src/withStyles.js
@@ -0,0 +1,60 @@
+import ThemedStyleSheet from "react-with-styles/lib/ThemedStyleSheet";
+import aphroditeInterface from "react-with-styles-interface-aphrodite/no-important";
+import { css, withStyles } from "react-with-styles";
+import * as THEME from "./MainTheme";
+
+ThemedStyleSheet.registerTheme({
+  /* Colors */
+  colors: {
+    success: THEME.COLOR_SUCCESS,
+    danger: THEME.COLOR_DANGER,
+    successHighlight: THEME.COLOR_SUCCESS_HIGHLIGHT,
+    dangerHighlight: THEME.COLOR_DANGER_HIGHLIGHT
+  },
+  /* Gradients */
+  gradients: {
+    success: THEME.GRADIENT_SUCCESS,
+    danger: THEME.GRADIENT_DANGER
+  },
+  /* themes */
+  themes: {
+    success: {
+      color: "white",
+      backgroundColor: THEME.COLOR_SUCCESS,
+      borderColor: THEME.COLOR_SUCCESS,
+      ":hover": {
+        backgroundColor: THEME.COLOR_SUCCESS_HIGHLIGHT
+      }
+    },
+    danger: {
+      color: "white",
+      borderColor: THEME.COLOR_DANGER,
+      backgroundColor: THEME.COLOR_DANGER,
+      ":hover": {
+        backgroundColor: THEME.COLOR_DANGER_HIGHLIGHT
+      }
+    },
+
+    successGradient: {
+      background: THEME.GRADIENT_SUCCESS,
+      ":active": {
+        background: THEME.GRADIENT_SUCCESS_INVERSE
+      },
+      ":hover": {
+        background: THEME.COLOR_SUCCESS_HIGHLIGHT
+      }
+    },
+    dangerGradient: {
+      background: THEME.GRADIENT_DANGER,
+      ":active": {
+        background: THEME.GRADIENT_DANGER_INVERSE
+      },
+      ":hover": {
+        background: THEME.COLOR_DANGER_HIGHLIGHT
+      }
+    }
+  }
+});
+ThemedStyleSheet.registerInterface(aphroditeInterface);
+
+export { css, withStyles, ThemedStyleSheet };
