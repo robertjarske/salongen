a5c090a5d05462989c3279d9ca7fcbc45837a5b4
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-05-24 21:52:20 +0200

    add comments, remove colors and themes not used

--- src/withStyles.js
+++ src/withStyles.js
@@ -3,18 +3,13 @@ import aphroditeInterface from "react-with-styles-interface-aphrodite/no-importa
 import { css, withStyles } from "react-with-styles";
 import * as THEME from "./MainTheme";
 
+/**Decided to implement airbnb's react-with-styles to be able to write styles as js objects,
+ this would serve a greater purpose in bigger projects since I can set themes much like mixins etc. */
+
 ThemedStyleSheet.registerTheme({
   /* Colors */
   colors: {
-    success: THEME.COLOR_SUCCESS,
-    danger: THEME.COLOR_DANGER,
-    successHighlight: THEME.COLOR_SUCCESS_HIGHLIGHT,
-    dangerHighlight: THEME.COLOR_DANGER_HIGHLIGHT
-  },
-  /* Gradients */
-  gradients: {
-    success: THEME.GRADIENT_SUCCESS,
-    danger: THEME.GRADIENT_DANGER
+    success: THEME.COLOR_SUCCESS
   },
   /* themes */
   themes: {
@@ -24,33 +19,6 @@ ThemedStyleSheet.registerTheme({
       borderColor: THEME.COLOR_SUCCESS,
       ":hover": {
         backgroundColor: THEME.COLOR_SUCCESS_HIGHLIGHT
-      }
-    },
-    danger: {
-      color: "white",
-      borderColor: THEME.COLOR_DANGER,
-      backgroundColor: THEME.COLOR_DANGER,
-      ":hover": {
-        backgroundColor: THEME.COLOR_DANGER_HIGHLIGHT
-      }
-    },
-
-    successGradient: {
-      background: THEME.GRADIENT_SUCCESS,
-      ":active": {
-        background: THEME.GRADIENT_SUCCESS_INVERSE
-      },
-      ":hover": {
-        background: THEME.COLOR_SUCCESS_HIGHLIGHT
-      }
-    },
-    dangerGradient: {
-      background: THEME.GRADIENT_DANGER,
-      ":active": {
-        background: THEME.GRADIENT_DANGER_INVERSE
-      },
-      ":hover": {
-        background: THEME.COLOR_DANGER_HIGHLIGHT
       }
     }
   },
